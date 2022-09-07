9ed918c05ef8d6d590d7d71c38d42eed02451b91
andrechacon <achacon190@gmail.com>
2022-09-06 22:03:25 -0300

    commit

---
+++ src/controllers/mainController.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const {json} = require('express');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		
+		let productos_ofer = products.filter( (e)=>{return e.discount > 1} );
+		let productos_sin_ofer = products.filter( (e)=>{return e.discount < 1 });
+		res.render('index',{ toThousand, productos_ofer, productos_sin_ofer });
+
+	},
+	search: (req, res) => {
+		 // // Do the magic
+	},
+};
+
+module.exports = controller;
